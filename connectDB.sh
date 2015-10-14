b994419e5579f176b34078330f83f8a229d83339
Anthony Kim <ak3703@columbia.edu>
2015-10-14 01:07:32 -0400

    fixed connectDB bash script to connect to mysql database

--- connectDB.sh
+++ connectDB.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mysql -h cs4111.caxmabhvo5cr.us-west-2.rds.amazonaws.com -P 3306 -u it2216 -p cs411
+mysql -h cs4111.caxmabhvo5cr.us-west-2.rds.amazonaws.com -P 3306 -u it2216 -p cs4111 
 
